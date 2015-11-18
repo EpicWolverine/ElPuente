1a98bcd83232d6b766038c20579191cc133965a6
EpicWolverine <epicwolverine@me.com>
2015-11-18 10:30:11 -0500

    Planned code structure

--- ElPuente/ElPuente.ino
+++ ElPuente/ElPuente.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+//byte IR1Pin = ;
+//byte warningLedPin = ;
 
+bool IR1State = false;
+bool IR1LastState = false;
+//bool IR2State = false;
+//bool IR2LastState = false;
+bool bridgeState = false; //false=lowered; true=raised
+
+void setup() {
+  //pinMode(IR1Pin, OUTPUT);
+  //pinMode(warningLedPin, OUTPUT);
+  
+  Serial.begin(9600);
+  Serial.println("El Puente!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //if(bridgeState=true){ //flash the lights
 
+  //check and store IR sensor 1
+  //check and store IR sensor 2
+
+  //if detection on IR sensor 1 (IR1State != IR1LastState && IR1State == true)
+    //raise bridge for x seconds (see how long it takes to raise)
+    //bridgeState = true
+  
+  //if detection on IR sensor 1 (IR1State != IR1LastState && IR1State == false)
+    //delay() //wait for train to finish passing
+    //lower bridge for x seconds (see how long it takes to lower)
+    //bridgeState = false
 }
